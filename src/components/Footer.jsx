75976e75c6df7098140d73d3c0c08ced126e11d9
Soham Desai <sohamdesai@Sohams-Mac-mini.local>
2025-01-07 13:16:09 -0500

    Added all right reserve with my name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="text-zinc-400 text-sm text-center p-4">
-      © {new Date().getFullYear()} All rights reserved.
+      © {new Date().getFullYear()} Soham Desai. All rights reserved.
     </footer>
   );
 }
